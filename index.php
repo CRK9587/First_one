d7c5b027e87c1b59df166841fae6448a945dec99
Chinmay Kulkarni <64392365+CRK9587@users.noreply.github.com>
2021-09-08 22:07:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <title>First_one</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="main1.css">
+    <!--<link rel="stylesheet" type="text/css" href="util1.css">
+    <link rel="stylesheet" href="main1.css">-->
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/0288478d4c.js" crossorigin="anonymous"></script>
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- Animate.CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  </head>
+  <body>
+      
+  </body>
+</html>
