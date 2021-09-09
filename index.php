8e81181d14d852a3e307f868a901788b3e28c9fa
Chinmay Kulkarni <64392365+CRK9587@users.noreply.github.com>
2021-09-09 07:52:11 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -20,7 +20,127 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   </head>
   <body>
-      <h1>Hello World..!</h1>
-      
+      <h1>Hello World</h1>
   </body>
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color: #1c2331"
+          >
+    <section
+             class="text-center text-lg-start text-white"
+             style="background-color: #6351ce;padding-top:3px;"
+             >
+      <div style="padding-left:360px">
+      <div class="row mt-3">
+          
+          <div class="me-6">
+            <span>Location</span>
+            <p><i class="fas fa-home mr-3"></i> Street Name</p>
+          </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+          <div class="me-6">
+            <span>Email</span>
+            <p><i class="fas fa-envelope mr-3"></i> abc@example.com</p>
+          </div>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+          <div class="me-6">
+            <span>Call</span>
+            <p><i class="fas fa-phone mr-3"></i> 987654321</p>
+          </div>
+
+      </div>
+      </div>
+    </section>
+
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <div class="row mt-3">
+
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+            <h6 class="text-uppercase fw-bold">Company name</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+            The region's leading professional service ..
+            </p>
+            <div>
+              <a href="" class="text-white me-4">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="" class="text-white me-4">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="" class="text-white me-4">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="" class="text-white me-4">
+                <i class="fab fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold">Navigation</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#!" class="text-white">Home</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">About</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Blog</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Contact Us</a>
+            </p>
+          </div>
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold">Services</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#!" class="text-white">Plumbing</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Carpentry</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">House painter and decorator</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Electricians</a>
+            </p>
+          </div>
+
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Working Hours</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>Mon - Wed&emsp;&emsp;9:00 am - 9:00 pm</p>
+            <p>Thu - Sat&emsp;&emsp;&emsp;9:00 am - 7:00 pm</p>
+            <p>Sunday&emsp;&emsp;&emsp;9:00 am - 2:30 am</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2020 Copyright:
+      <a class="text-white" href="#"
+         ></a
+        >
+    </div>
+</footer>
+
 </html>
